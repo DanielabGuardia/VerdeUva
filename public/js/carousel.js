1960ae51f728bf0ba5397426d6315deb1bbe2ab3
DanielabGuardia <danielabguardia@gmail.com>
2022-12-12 10:52:08 -0300

    home

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -3,3 +3,8 @@ const myCarousel = document.getElementById('myCarousel')
 myCarousel.addEventListener('slide.bs.carousel', event => {
   // do something...
 })
+const myCarouselElement = document.querySelector('#myCarousel')
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 5000,
+  wrap: false
+})
